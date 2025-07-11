7da758dac7ec97fa641ad2c963d6ecb18fce86e1
Marc Bejerano <marcbejerano@gmail.com>
2025-07-11 08:52:31 -0700

    Minor changes to increase readability

--- Objs.py
+++ Objs.py
@@ -1,7 +1,11 @@
 import json
 
 class Quest:
-    _next_id = 1 # Class-level counter for unique IDs
+    __next_id : int  = 1 # Class-level counter for unique IDs
+    id_num    : int = 1
+    name      : str = None
+    exp       : int = 0
+    complete  : bool = False
 
     def __init__(self, name, exp=0):
 
@@ -10,23 +14,24 @@ class Quest:
         self.exp = exp
         self.complete = False
 
-    def __str__(self):
+    def __str__(self) -> str:
         # Returns the quest name and XP for listing purposes
         return f"{self.name}     XP: {self.exp}"
 
-    def __int__(self):
+    def __int__(self) -> int:
         # Returns the id quest id number
         return self.id_num
 
-    def status_check(self):
+    def is_complete(self) -> bool:
         # Returns the Boolean state for completion
         return self.complete
 
-    def to_dict(self):
-        # Converts the Quest instance to a dictionary
-        return {
-            "id_num": self.id_num,
-            "name": self.name,
-            "exp": self.exp,
-            "complete": self.complete
-        }
+    #def to_dict(self):
+    #    # Converts the Quest instance to a dictionary
+    #    return {
+    #        "id_num": self.id_num,
+    #        "name": self.name,
+    #        "exp": self.exp,
+    #        "complete": self.complete
+    #    }
+    
